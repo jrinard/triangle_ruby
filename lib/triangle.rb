3bf0d15499459189a4c94df71737c1f12cdf3f9d
Joshua Rinard and Marty Kovach <joshua@>
2016-11-28 09:14:59 -0800

    logic for isosceles

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -6,6 +6,13 @@ class Triangle
   end
 
   define_method(:triangle?) do
-    @side1.eql?(@side2) and @side2.eql?(@side3)
+    if @side1.eql?(@side2) and @side2.eql?(@side3)
+      triangle = "equilateral"
+    elsif (@side1.eql?(@side2) and @side2 != @side3) or
+          (@side1.eql?(@side3) and @side2 != @side3) or
+          (@side2.eql?(@side3) and @side2 != @side1)
+      triangle = "isosceles"
+  end
+  "is an " + triangle.to_s()
   end
 end
--- spec/triangle_spec.rb
+++ spec/triangle_spec.rb
@@ -3,13 +3,17 @@ require('triangle')
 
 describe(Triangle) do
   describe('#triangle?') do
-    it('returns false if it is not an equilateral') do
-      test_triangle = Triangle.new(30,30,20)
-      expect(test_triangle.triangle?()).to(eq(false))
-    end
+    # it('returns not an equilateral') do
+    #   test_triangle = Triangle.new(30,35,20)
+    #   expect(test_triangle.triangle?()).to(eq("NOT an equilateral"))
+    # end
     it('returns true if it is an equilateral') do
       test_triangle = Triangle.new(30,30,30)
-      expect(test_triangle.triangle?()).to(eq(true))
+      expect(test_triangle.triangle?()).to(eq("is an equilateral"))
+    end
+    it('returns true if is an isosceles') do
+      test_triangle = Triangle.new(30,30,10)
+      expect(test_triangle.triangle?()).to(eq("is an isosceles"))
     end
   end
 end
